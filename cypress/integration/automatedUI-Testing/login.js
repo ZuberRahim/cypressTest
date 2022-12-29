251637c15f360860bcc62a9a8bd69d667b6ce2bb
ZuberRahim <zuber.rahim@ezyvet.com>
2022-12-29 14:32:33 +1300

    29/12/22

--- cypress/integration/automatedUI-Testing/login.js
+++ cypress/integration/automatedUI-Testing/login.js
@@ -17,6 +17,24 @@ describe("AutoLogin", () => {
     // cy.get('[data-testid^="PatientListCard_').eq(0).click();
     // cy.get('[data-testid="Grid"]').should("be.visible");
   });
+  it("Create a Hospital Sheet", () => {
+    cy.login("team@vetradar.com", "Spacedog123");
+    cy.get('[data-testid^="PatientListCard_').eq(0).click();
+    cy.get('[data-testid="Grid"]').should("be.visible");
+    cy.get('[data-testid="HeaderHeadline"]').click();
+    cy.contains("+ Create sheet").click();
+    cy.get('[data-testid="SheetNameInput"]').then(($sheetName) => {
+      const sheetInput = $sheetName.val();
+      cy.log(sheetInput);
+
+      cy.get('[data-testid="addSheetButton"]').click();
+      cy.get('[data-testid="HeaderHeadline"]').then(($newSheetName) => {
+        const newSheetName = $newSheetName.text();
+        cy.log(newSheetName);
+        expect(newSheetName).to.include(sheetInput);
+      });
+    });
+  });
 });
 
 //I am adding this here as a test
